749b865c35452f3a25fbe77be8450f971b7a2ffd
wangnan0610 <375584419@qq.com>
2014-10-30 22:36:33 +0800

    starry_sky

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,37 @@
+//引入gulp
 var gulp = require('gulp');
 
+// 引入组件
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var handleError = function(err) { console.log(err); if (this.emit) { this.emit('end'); } };
+
+//test
+gulp.task('test', function() {
+    return gulp.src('./snippets/starry_sky/starry_sky.scss')
+    .pipe(plumber())
+    .pipe(sass({ onError: handleError }))
+    .pipe(gulp.dest('./snippets/starry_sky/starry_sky.css'));
+});
+
+//编译sass
+gulp.task('sass', function() {
+    // place code for your default task here
+    gulp.src('./snippets/starry_sky/starry_sky.scss')
+    	.pipe(sass())
+        .pipe(gulp.dest('./snippets/starry_sky'));
+});
+
 gulp.task('default', function() {
-      // place code for your default task here
+    //
+});
+
+//监听sass
+gulp.task('watch', function() {
+    gulp.watch('./snippets/starry_sky/starry_sky.scss',['sass']);
 });
